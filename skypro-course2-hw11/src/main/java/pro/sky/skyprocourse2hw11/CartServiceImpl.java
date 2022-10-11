aa471d650dd4226c4f8980266b61216be98d3dae
Ekaterina Gorbacheva <katerina.smr@gmail.com>
2022-10-11 22:34:56 +0300

    hw for lesson 2.11 Spring - updates based on comments

--- skypro-course2-hw11/src/main/java/pro/sky/skyprocourse2hw11/CartServiceImpl.java
+++ skypro-course2-hw11/src/main/java/pro/sky/skyprocourse2hw11/CartServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Service
 public class CartServiceImpl implements CartService {
-    private Cart cart;
+    private final Cart cart;
 
     public CartServiceImpl(Cart cart){
         this.cart = cart;
